f65375d90348e6119756dc7e39aae182511c100e
Mozikr <szymonmozol123@gmail.com>
2022-06-13 18:14:02 +0200

    Commit2

---
+++ azurecred.py
@@ -0,0 +1,5 @@
+AZDBSERVER = 'mozolbaza.database.windows.net' #twoja nazwa utworzonego serwera
+AZDBNAME = 'mozolbaza' #twoja nazwa bazy na serwerze
+AZDBUSER = 'Mozol' #twoja nazwa użytkownika
+AZDBPW = 'Szymon12309856' #twoje hasło,którego nie powinniśmy znać
+AZDBDRIVER = '{ODBC Driver 17 for SQL Server}' #sterownik bazy danych, nic nie zmieniaj
